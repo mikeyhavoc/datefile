dc1176e6bba3e0038e1be92c4ca5f19b4007a208
mikey <mikeyjhavoc@gmail.com>
2016-02-16 11:55:36 -0500

    eliminate none-needed imports, add def file_type, def file_change - file startswith 'year' should skip file, TODO if file endswith specific filetype os.rename specific files with date_name

--- date.py
+++ date.py
@@ -6,10 +6,10 @@
 # version 1. gets date file.
 # version 2. name files if want too.
 # version 3. get creation date for file.
-# update: 2/15/16
-# Version: 1.1.5
+# update: 2/16/16
+# Version: 2.0
 
-import datetime, sys, os, re
+import datetime, os
 
 #Get year, month, day, minutes
 year = datetime.datetime.today().year
@@ -17,19 +17,28 @@ month = datetime.datetime.today().month
 day = datetime.datetime.today().day
 minute = datetime.datetime.today().minute
 
+def file_type():
+    '''Lets the user select the file type for which they will be replacing the date on'''
+    kind = input('search for what kind of file: ')
+    ext = '.'
+    ext += kind
+    return ext
+
+
 #Gets files in folder, replaces files with yyyy-mm-dd-filename.ext
 def file_change(folder):
     for filenames in os.listdir(folder):
-        date_name = str(year) + '-' + str(month) + '-' + str(day) + '-' + str(minute) + "_" + filenames
+        date_name = str(year) + '-' + str(month) + '-' + str(day) + "_" + filenames
 
         if filenames.startswith('date') and filenames.endswith('.py'):
             continue    # skip date.py file
         if filenames.startswith('.git') and filenames.endswith('.git'):
             continue    # skips git repository
+        if filenames.startswith(str(year)):
+            continue    # skips previously dated files.
         if filenames.endswith('.txt'):
-            if filenames.startswith(str(year)):
-                continue    # skips previously dated files.
-        os.rename(filenames, date_name)
+            os.rename(filenames, date_name)
+
 
 
 file_change('C:\\Users\\mjwil\\Documents\\portfolio\\python\\date')
